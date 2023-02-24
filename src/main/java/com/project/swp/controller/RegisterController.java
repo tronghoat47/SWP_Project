ca7561ddc1ca8a293ca63c82acae4a7fb93ac35e
Nguyễn Hồng Sơn <son29062002@gmail.com>
2023-02-24 16:08:45 +0700

    son

--- src/main/java/com/project/swp/controller/RegisterController.java
+++ src/main/java/com/project/swp/controller/RegisterController.java
@@ -4,7 +4,7 @@ import com.project.swp.DAO.CustomerDAO;
 import com.project.swp.entity.Customer;
 import com.project.swp.repository.CustomerRepo;
 import com.project.swp.service.CustomerService;
-import jakarta.servlet.http.HttpSession;
+//import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +19,11 @@ public class RegisterController {
     private CustomerService customerService;
 
     @PostMapping("/customer")
-    public String login(@RequestBody Customer customer, HttpSession session){
+    public String login(@RequestBody Customer customer){
         Customer checkCus = customerService.createCustomer(customer);
 
         if(checkCus!=null){
-            session.setAttribute("user", checkCus);
+//            session.setAttribute("user", checkCus);
             return "redirect:/error";
         }else {
             return "redirect:/home";
--- src/main/java/com/project/swp/repository/CompanyRepo.java
+++ src/main/java/com/project/swp/repository/CompanyRepo.java
@@ -1,8 +1,13 @@
 package com.project.swp.repository;
 
 import com.project.swp.entity.Company;
+import com.project.swp.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CompanyRepo extends JpaRepository<Company, Integer> {
+    Optional<List<Company>> findCompaniesByCompanyName(String username);
 
 }
